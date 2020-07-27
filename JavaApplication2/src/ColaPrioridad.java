82e83919a571968aead2ba9c11b1650b5b515d05
marianaf17 <nicoldv2@gmail.com>
2020-07-27 12:21:39 -0400

    colita terminada

--- JavaApplication2/src/ColaPrioridad.java
+++ JavaApplication2/src/ColaPrioridad.java
@@ -10,7 +10,7 @@ public class ColaPrioridad{
     private Queue<Solicitud>[]colas;
     private ArrayList<Solicitud> solicitudes;
     private  int numPrioridades =3;
-    
+
     public ColaPrioridad(){
         solicitudes=new ArrayList<>();
         colas=new PriorityQueue[numPrioridades]; 
@@ -39,29 +39,47 @@ public class ColaPrioridad{
             cola.add(s);
         }else{
             Queue<Solicitud> cola1 = new PriorityQueue<Solicitud>(); 
+
             colas[prioridad-1]=cola1;
             cola1.add(s);
             prioridades.add(prioridad);
         }
     }
 
-    public Queue<Solicitud>[] getColas(){
+    private Queue<Solicitud>[] getColas(){
         return colas;
     }
 
-    public void mostrarLista() throws IOException{
-        retornarSolicitudes();
-        //aqui te falta poner algo  xd
+    
+
+    public   void main() throws IOException{
+         llenar();
+
         for(int i=0;i<colas.length;i++){
             Queue<Solicitud> cola=colas[i];
-            imprimir(cola);
+            if(cola!=null){
+                imprimir(cola);
+            }
         }
     }
 
-    public static void imprimir( Queue<Solicitud> cola){
+    private void imprimir( Queue<Solicitud> cola){
         for(Solicitud s : cola) {
             Producto p=s.getProduc();
             System.out.println(p.getNom()+ " --> " + s.getCant() + "  -- [ " + p.getPrioridad() + "]");
+        }
+    }
+
+    private   void llenar()throws IOException {
+        try {
+            retornarSolicitudes();
+            for(Solicitud s:solicitudes){
+                añadirColaPrioridad (s);
+
+            }
+
+        }
+        catch (IOException e) {
         }
     }
 }
